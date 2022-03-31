959ea37497bebacebf607818e3daf049b9ef4849
carlosdiaztosina <calosdtpiopio@gmail.com>
2022-03-31 12:32:49 +0100

    calculadora

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angularCalculadora'`, () => {
+  it(`should have as title 'angularTexto'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angularCalculadora');
+    expect(app.title).toEqual('angularTexto');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angularCalculadora app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angularTexto app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularCalculadora';
+  title = 'Calculadora';
+  text1="1";
+  text2="2";
+  concat='X';
+  concat2:Number=0;
+
+  concatenate():void{
+    this.concat =this.text1 + this.text2;
+  }
+  suma():void{
+    this.concat2 =Number(this.text1) + Number(this.text2);
+  }
+  resta():void{
+    this.concat2 =Number(this.text1) - Number(this.text2);
+  }
+  multi():void{
+    this.concat2 =Number(this.text1) * Number(this.text2);
+  }
+  divi():void{
+    this.concat2 =Number(this.text1) / Number(this.text2);
+  }
 }
